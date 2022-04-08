47b8ebadc36fdd1ca63176775584a93cee3d3184
Abenezer <abeniabena1@gmail.com>
2022-04-08 13:32:03 -0700

    alx tasks

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print alphabet 
+ * main - print alphabet
  *
  * Return: Always (Success)
  */
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
 
