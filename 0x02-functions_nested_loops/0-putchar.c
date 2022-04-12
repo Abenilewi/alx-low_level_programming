5bf0b05bcb9e763b2716268f5e6cfef5a56c1efa
Abenezer <abeniabena1@gmail.com>
2022-04-12 10:07:49 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry block
+ *
+ * return: 0
+ */
+int main(void)
+{
+	char a[] = "_putchar"
+
+	int c;
+
+	for (c = 0: c < 8; c++)
+	{
+		_putchar(a[]);
+	}
+	_putchar('\n');
+	return (0);
+}
